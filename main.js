caa35be9c17fbe199cbe9aa11da8dc8fafc14e96
SondaSengupta <sondasengupta@gmail.com>
2017-11-21 16:57:17 -0600

    Added pics and first part of page

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+// (function(){
+   
+
+    console.log("js is go!")
+    // $("#SubmitButton").click(function(){
+    //     $('#CharacterDiv').prepend('<img id="theImg" src="Pics/Dustin.jpg" />')
+    //    })
+    
+    function submitQuiz() {
+        console.log("Hi");
+    }
+
+   
+   
+// }());
